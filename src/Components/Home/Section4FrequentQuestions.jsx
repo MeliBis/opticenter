de4bccfbb1f19f11f53c507aa0f5f3260688767a
Meli Barrios <melissa.blecona@gmail.com>
2024-02-06 15:59:37 -0600

    se agrega seccion de preg frecuentes

--- src/Components/Home/Section4FrequentQuestions.jsx
+++ src/Components/Home/Section4FrequentQuestions.jsx
@@ -1,7 +1,133 @@
 
 const Section4FrequentQuestions = () => {
   return (
-    <div>Section4FrequentQuestions</div>
+    <main className=" bg-light d-flex aling-items-center mb-5">
+    <section className="h-75 container-sm mx-auto">
+      <h2 className=" text-center py-3">
+        Preguntas frecuentes
+      </h2>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+           
+
+              <span className="px-3 questionSection">Pregunta #1</span>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>Respuesta 1</p>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              <span className="px-3 questionSection">Pregunta #2</span>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>Respuesta 2</p>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              <span className="px-3 questionSection">Pregunta #3</span>
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>Respuesta 3</p>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+            >
+              <span className="px-3 questionSection">Pregunta #4</span>
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>Respuesta 4</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFive"
+              aria-expanded="false"
+              aria-controls="collapseFive"
+            >
+              <span className="px-3 questionSection">Pregunta #5</span>
+            </button>
+          </h2>
+          <div
+            id="collapseFive"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>Respuesta 5</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
   )
 }
 
