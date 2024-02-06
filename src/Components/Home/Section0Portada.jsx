4802732218ebb4ed4787633c687e5358cb2631a8
Meli Barrios <melissa.blecona@gmail.com>
2024-02-06 15:50:29 -0600

    se agregan estilos a seccion portada

--- src/Components/Home/Section0Portada.jsx
+++ src/Components/Home/Section0Portada.jsx
@@ -1,7 +1,14 @@
 
+
 const Section0Portada = () => {
   return (
-    <div>Section0Portada</div>
+    <header className="masthead">
+    <div className="container">
+      <p className="masthead-subheading">Bienvenidos a Opti Center</p>
+      <div className="masthead-heading  fs-2">¡Es un gusto verte !</div>
+      <a className="btn btn-primary btn-xl " href="#services">Conócenos</a>
+    </div>
+</header> 
   )
 }
 
