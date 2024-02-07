7c4fc6853909335f7a7893e058160adfdc36f224
Meli Barrios <melissa.blecona@gmail.com>
2024-02-07 16:25:26 -0600

    se agregan iconos seccion productos

--- src/Components/Pages/ProductsPage.jsx
+++ src/Components/Pages/ProductsPage.jsx
@@ -1,11 +1,14 @@
 import GalleryProducts from "../Products-services/GalleryProducts"
+import IconosProducts from "../Products-services/IconosProducts"
 import IntroProducts from "../Products-services/IntroProducts"
 
 const ProductsPage = () => {
   return (
     <div>
       <IntroProducts/>
+      <IconosProducts/>
       <GalleryProducts/>
+
     </div>
   )
 }
---
+++ src/Components/Products-services/IconosProducts.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom'
+import iconosProduct1 from '../../assets/products/gafas.png'
+import iconosProduct2 from '../../assets/products/test.png'
+
+
+const IconosProducts = () => {
+  return (
+    <div className="row text-center mt-5 d-flex justify-content-center">
+    <div className="col-md-4">
+      <span className="fa-stack fa-4x">
+        <Link to='/productos'>
+          <img src={iconosProduct1} width={100} className="" alt="" />
+        </Link>
+       </span>
+      <h4 className="my-3">Los mejores armazones <br /><span className='text-secondary'>a tu alcance</span></h4>
+      <p className="text-muted">
+      </p>
+    </div>
+    <div className="col-md-4">
+      <span className="fa-stack fa-4x">
+        <Link to='/productos'>
+          <img src={iconosProduct2} width={100} className="" alt="" />
+        </Link>
+      </span>
+      <h4 className="my-3">Primer exámen de la vista <br /><span className="text-secondary">Gratis</span></h4>
+      <p className="text-muted">
+      </p>
+    </div>
+ 
+  </div>
+  )
+}
+
+export default IconosProducts
