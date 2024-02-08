8fdaf1d6de60f80b4b745a5f7029b95db0f9a341
Meli Barrios <melissa.blecona@gmail.com>
2024-02-08 08:34:06 -0600

    se agregan imagenes a cards de testimonios

--- src/Components/Home/Section3Testimonials.jsx
+++ src/Components/Home/Section3Testimonials.jsx
@@ -1,36 +1,55 @@
 /* import data from '../Data/testimonials'
- */import imgTestimonial from '../../assets/testimonials/mel.jpeg'
+ */
+import imgTestimonialMel from "../../assets/testimonials/meli.jpg";
+import imgTestimonialCata from "../../assets/testimonials/cata.jpg";
+ import imgTestimonialIbhar from "../../assets/testimonials/ibhar.jpg";
 
 const Section3Testimonials = () => {
   return (
     <div className=" container d-flex justify-content-center margen">
-{/*       <h2 className='text-center'>Testimonios</h2>
- */}      <div
+      {/*       <h2 className='text-center'>Testimonios</h2>
+       */}{" "}
+      <div
         id="carouselExampleAutoplaying"
         className="carousel slide  text-center margen"
         data-bs-ride="carousel"
       >
         <div className="carousel-inner  ">
           <div className="carousel-item active ">
-            <div className="card p-3" style={{ width: "30rem" }}>
-              <img src='' className=" img-" alt="..." />
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={imgTestimonialCata} className="card-img-top" alt="..." />
               <div className="card-body">
-              <h5 className='text-secondary mb-1'>Melissa Barrios</h5>
-              <h6 className='text-secondary'>Zacatlán, Puebla</h6>
-
-                <p className="text-secondary">
-                Hace unos meses comencé a tener problemas de visión y decidí visitar Opti Center. Desde el primer momento, me sentí muy bien atendida. Me realizaron un examen visual completo y me explicaron con detalle las diferentes opciones que tenía para corregir mi vista.
+                <h5 className="card-title">Cata Micaela Barrios</h5>
+                <p className="card-text">
+                Tuve que graduarme la vista con urgencia y acudí a ÓpticaVisión. Me atendieron con gran profesionalidad y rapidez. En menos de una hora me habían realizado el examen visual y me habían dado las gafas graduadas.
                 </p>
-                
+             
               </div>
             </div>
           </div>
           <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
-            
+          <div className="card" style={{ width: "18rem" }}>
+              <img src={imgTestimonialMel} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Melissa Barrios</h5>
+                <p className="card-text">
+                Hace unos meses que comencé a tener problemas de visión y decidí visitar Opti Center. Desde el primer momento, me sentí muy bien atendido por el equipo de profesionales. Me realizaron un examen visual completo y me explicaron con detalle las diferentes opciones que tenía para corregir mi vista.
+                </p>
+             
+              </div>
+            </div>
           </div>
           <div className="carousel-item">
-            <img src="..." className="d-block w-100" alt="..." />
+          <div className="card" style={{ width: "18rem" }}>
+              <img src={imgTestimonialIbhar} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Ibhar Jimenez</h5>
+                <p className="card-text">
+                Visité Opti Center buscando unas gafas nuevas y me encantó la atención personalizada que recibí. El personal me dedicó todo el tiempo necesario para asesorarme sobre las diferentes opciones que tenía disponibles, teniendo en cuenta mis necesidades y presupuesto.
+                </p>
+               
+              </div>
+            </div>
           </div>
         </div>
         <button
